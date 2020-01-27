09f205689c60c9149b0e5ba257fae83d55cd768c
Pozharov Roman <ru.roman.ps@gmail.com>
2020-01-27 05:54:07 +0400

    Добавлены переменные в main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const FORM = document.querySelector(".form");
+const MESSAGE = document.querySelector(".form__message");
+const MAIL = document.querySelector(".form__mail");
+
+let infoSubmit = [];
+let infoToJSON = [];
+
+function addReview() {
+  let last = {message: MESSAGE.value, mail: MAIL.value};
+  infoSubmit.push(last);
+  toLocalStorage(infoSubmit);
+}
+
+FORM.addEventListener("submit", addReview);
